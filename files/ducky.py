87904e64e01ea214170cc7d59c97da03956dca15
Bill Sideris <bill88t@feline.gr>
2024-02-25 21:38:02 +0200

    add next version wip

--- files/ducky.py
+++ files/ducky.py
@@ -1,133 +1,27 @@
+rename_process("ducky")
+if cptoml.fetch("usb_hid_available", "LJINUX"):
+    if cptoml.fetch("usb_hid_enabled", "LJINUX"):
+        ljinux.api.subscript("/bin/ducky/duckyload.py")
+    else:
+        term.write(
+            "usb_hid is not enabled in `&/settings.toml`!\n"
+            + "To enable it, set `usb_hid_enabled` to 'true'."
+        )
+else:
+    term.write("This board does not support usb_hid!")
 try:
+    vr("opts", ljinux.api.xarg())
+    if "h" in vr("opts")["o"] or "help" in vr("opts")["o"]:
+        ljinux.based.run("cat /")
+    else:
+        if len(vr("opts")["w"]):
+            pass
+        else:
+            pass
     if ljinux.api.isdir(ljinux.based.user_vars["argj"].split()[1], rdir=getcwd()):
         ljinux.based.error(4, ljinux.based.user_vars["argj"].split()[1])
         ljinux.based.user_vars["return"] = "1"
     else:
-        from usb_hid import devices
-        from adafruit_hid.keyboard import Keyboard
-        from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
-        from adafruit_hid.keycode import Keycode
-        from time import sleep
-        from sys import modules
-
-        duckyCommands = {
-            "WINDOWS": Keycode.WINDOWS,
-            "GUI": Keycode.GUI,
-            "APP": Keycode.APPLICATION,
-            "MENU": Keycode.APPLICATION,
-            "SHIFT": Keycode.SHIFT,
-            "ALT": Keycode.ALT,
-            "CONTROL": Keycode.CONTROL,
-            "CTRL": Keycode.CONTROL,
-            "DOWNARROW": Keycode.DOWN_ARROW,
-            "DOWN": Keycode.DOWN_ARROW,
-            "LEFTARROW": Keycode.LEFT_ARROW,
-            "LEFT": Keycode.LEFT_ARROW,
-            "RIGHTARROW": Keycode.RIGHT_ARROW,
-            "RIGHT": Keycode.RIGHT_ARROW,
-            "UPARROW": Keycode.UP_ARROW,
-            "UP": Keycode.UP_ARROW,
-            "BREAK": Keycode.PAUSE,
-            "PAUSE": Keycode.PAUSE,
-            "CAPSLOCK": Keycode.CAPS_LOCK,
-            "DELETE": Keycode.DELETE,
-            "END": Keycode.END,
-            "ESC": Keycode.ESCAPE,
-            "ESCAPE": Keycode.ESCAPE,
-            "HOME": Keycode.HOME,
-            "INSERT": Keycode.INSERT,
-            "NUMLOCK": Keycode.KEYPAD_NUMLOCK,
-            "PAGEUP": Keycode.PAGE_UP,
-            "PAGEDOWN": Keycode.PAGE_DOWN,
-            "PRINTSCREEN": Keycode.PRINT_SCREEN,
-            "ENTER": Keycode.ENTER,
-            "SCROLLLOCK": Keycode.SCROLL_LOCK,
-            "SPACE": Keycode.SPACE,
-            "TAB": Keycode.TAB,
-            "BACKSPACE": Keycode.BACKSPACE,
-            "F1": Keycode.F1,
-            "F2": Keycode.F2,
-            "F3": Keycode.F3,
-            "F4": Keycode.F4,
-            "F5": Keycode.F5,
-            "F6": Keycode.F6,
-            "F7": Keycode.F7,
-            "F8": Keycode.F8,
-            "F9": Keycode.F9,
-            "F10": Keycode.F10,
-            "F11": Keycode.F11,
-            "F12": Keycode.F12,
-            "A": Keycode.A,
-            "B": Keycode.B,
-            "C": Keycode.C,
-            "D": Keycode.D,
-            "E": Keycode.E,
-            "F": Keycode.F,
-            "G": Keycode.G,
-            "H": Keycode.H,
-            "I": Keycode.I,
-            "J": Keycode.J,
-            "K": Keycode.K,
-            "L": Keycode.L,
-            "M": Keycode.M,
-            "N": Keycode.N,
-            "O": Keycode.O,
-            "P": Keycode.P,
-            "Q": Keycode.Q,
-            "R": Keycode.R,
-            "S": Keycode.S,
-            "T": Keycode.T,
-            "U": Keycode.U,
-            "V": Keycode.V,
-            "W": Keycode.W,
-            "X": Keycode.X,
-            "Y": Keycode.Y,
-            "Z": Keycode.Z,
-            "a": Keycode.A,
-            "b": Keycode.B,
-            "c": Keycode.C,
-            "d": Keycode.D,
-            "e": Keycode.E,
-            "f": Keycode.F,
-            "g": Keycode.G,
-            "h": Keycode.H,
-            "i": Keycode.I,
-            "j": Keycode.J,
-            "k": Keycode.K,
-            "l": Keycode.L,
-            "m": Keycode.M,
-            "n": Keycode.N,
-            "o": Keycode.O,
-            "p": Keycode.P,
-            "q": Keycode.Q,
-            "r": Keycode.R,
-            "s": Keycode.S,
-            "t": Keycode.T,
-            "u": Keycode.U,
-            "v": Keycode.V,
-            "w": Keycode.W,
-            "x": Keycode.X,
-            "y": Keycode.Y,
-            "z": Keycode.Z,
-            "0": Keycode.ZERO,
-            "1": Keycode.ONE,
-            "2": Keycode.TWO,
-            "3": Keycode.THREE,
-            "4": Keycode.FOUR,
-            "5": Keycode.FIVE,
-            "6": Keycode.SIX,
-            "7": Keycode.SEVEN,
-            "8": Keycode.EIGHT,
-            "9": Keycode.NINE,
-        }
-
-        kbd = Keyboard(devices)
-        del devices
-        layout = KeyboardLayoutUS(kbd)
-        defaultDelay = 60
-
-        dataa = None
-
         with open(
             ljinux.api.betterpath(ljinux.based.user_vars["argj"].split()[1]), "r"
         ) as f:
@@ -180,6 +74,5 @@ try:
             cl += 1
         del duckyCommands, dataa, lc, cl, kbd, layout
         del KeyboardLayoutUS, Keycode, defaultDelay
-
 except IndexError:
     ljinux.based.error(1)
