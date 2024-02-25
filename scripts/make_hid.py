c1f8075f07303422751e472cb93628baad1b8a99
Bill Sideris <bill88t@feline.gr>
2024-02-25 21:37:23 +0200

    Update circuitmpy commands

--- scripts/make_hid.py
+++ scripts/make_hid.py
@@ -10,6 +10,7 @@ def errexit():
 spath.append("./submodules/CircuitMPY/")
 import circuitmpy
 
+circuitmpy.fetch_mpy()
 path = "./submodules/Adafruit_CircuitPython_HID/adafruit_hid/"
 
 for filee in listdir(path):
